6103b8b490c7858f8d2ced927cea0267cb55aa4a
Alexander Tu <alexander.md.tu@gmail.com>
2019-09-19 15:50:39 +0200

    Started Project2, set up button layout

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Alexander Tu on 19.09.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
