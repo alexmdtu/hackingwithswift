7c9f4437e0b7c1a7723dc2495274573c99d38df9
Alexander Tu <alexander.md.tu@gmail.com>
2019-09-21 23:47:44 +0200

    Init Challenge1

---
+++ Milestone1/Milestone1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milestone1
+//
+//  Created by Alexander Tu on 21.09.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
