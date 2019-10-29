c4ab3a5ba57c640771348911a694717b5238e21b
Alexander Tu <alexander.md.tu@gmail.com>
2019-10-30 00:16:34 +0100

    started Project7

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Alexander Tu on 30.10.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
