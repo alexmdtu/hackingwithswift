590c18aecd87ad1444fa175273640663bcbcc8b4
Alexander Tu <alexander.md.tu@gmail.com>
2019-10-30 00:06:12 +0100

    Finished milestone3 project

--- Milestone3/Milestone3/ViewController.swift
+++ Milestone3/Milestone3/ViewController.swift
@@ -8,13 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var shoppingItems = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Shopping List"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(emptyList))
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingItems[indexPath.row]
+        return cell
+    }
+    
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Enter item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.submit(item)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    @objc func emptyList() {
+        shoppingItems.removeAll()
+        tableView.reloadData()
+    }
+    
+    func submit(_ item: String) {
+        shoppingItems.insert(item, at: 0)
+        let indexPath = IndexPath(item: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
 }
 
