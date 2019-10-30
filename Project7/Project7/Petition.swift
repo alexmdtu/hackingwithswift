187545161dc3b335242cb631c71735836601ad47
blinkVR <pianoguitarify123@gmail.com>
2019-10-30 14:05:13 +0100

    Finished project7

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Alexander Tu on 30.10.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
