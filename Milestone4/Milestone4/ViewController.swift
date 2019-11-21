9e21c874f176dd636673c23669df6f11b0132075
Alexander Tu <alexander.md.tu@gmail.com>
2019-11-21 15:22:45 +0100

    started Milestone4 project

---
+++ Milestone4/Milestone4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milestone4
+//
+//  Created by Alexander Tu on 21.11.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
