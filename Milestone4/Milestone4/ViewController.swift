35b3c515b82c84588be810f356aa65c0cdd5b82b
Alexander Tu <alexander.md.tu@gmail.com>
2019-11-22 11:39:21 +0100

    added question file

--- Milestone4/Milestone4/ViewController.swift
+++ Milestone4/Milestone4/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var cluesLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var level = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // setup letter buttons, take similar approach like in swifty words game
+        view = UIView()
+        view.backgroundColor = .white
+        
+        // load level
     }
 
 
