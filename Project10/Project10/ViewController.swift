2130afe3b294e1c1de67e2704aa74782062455bc
Alexander Tu <alexander.md.tu@gmail.com>
2019-11-23 16:38:17 +0100

    added prompt for removal for challenge 1

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -68,6 +68,8 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
         
+        
+        
         let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
         ac.addTextField()
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
@@ -78,7 +80,17 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
             self?.collectionView.reloadData()
         }))
         
-        present(ac, animated: true)
+        let initialAc = UIAlertController(title: "Remove or Rename?", message: "What do you want to do?", preferredStyle: .alert)
+        initialAc.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        initialAc.addAction(UIAlertAction(title: "Rename", style: .default, handler: { _ in
+            self.present(ac, animated: true)
+        }))
+        initialAc.addAction(UIAlertAction(title: "Remove", style: .destructive, handler: { _ in
+            self.people.remove(at: indexPath.item)
+            collectionView.reloadData()
+        }))
+        
+        present(initialAc, animated: true)
     }
     
     @objc func addNewPerson() {
