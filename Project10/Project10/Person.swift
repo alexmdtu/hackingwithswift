7ae02b942b1e11fc91c72d5767851dcb8704192e
Alexander Tu <alexander.md.tu@gmail.com>
2019-11-23 16:27:35 +0100

    finished project10

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Alexander Tu on 23.11.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+ 
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
