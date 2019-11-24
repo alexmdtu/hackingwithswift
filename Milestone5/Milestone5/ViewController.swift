25a1b5ae58ee27bca1e8ab919acf02a0a9db19bd
Alexander Tu <alexander.md.tu@gmail.com>
2019-11-24 22:14:11 +0100

    started milestone5 project

---
+++ Milestone5/Milestone5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milestone5
+//
+//  Created by Alexander Tu on 24.11.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
