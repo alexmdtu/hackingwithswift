8ad470de77d31c74546e017876f5bd3d6a6983a4
Alexander Tu <alexander.md.tu@gmail.com>
2019-11-25 00:03:40 +0100

    initial setup in storyboard

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func changeFilter(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+        
+    }
 }
 
