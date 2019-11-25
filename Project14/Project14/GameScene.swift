c7471f65f265f9640f0351cc409c924f4b7378b3
Alexander Tu <alexander.md.tu@gmail.com>
2019-11-25 23:42:50 +0100

    started project14 and cleaned up initial setup

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Alexander Tu on 25.11.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
