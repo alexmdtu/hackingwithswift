fac34b9d7d388b35aed4d6ae99a7aafe9dfd5738
Alexander Tu <alexander.md.tu@gmail.com>
2019-11-27 23:36:08 +0100

    started project17, cleaned up files and added assets

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Alexander Tu on 27.11.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
