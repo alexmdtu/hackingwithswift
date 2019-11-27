9fa74e0f0ede4ef4c6fbd0645e45418d83043ebb
Alexander Tu <alexander.md.tu@gmail.com>
2019-11-27 16:36:55 +0100

    started project16

---
+++ Project16/Project16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Alexander Tu on 27.11.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
