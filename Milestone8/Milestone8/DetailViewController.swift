93bcc8f0ad9861681fc0fc7a22c8a158e76be548
Alexander Tu <alexander.md.tu@gmail.com>
2019-12-02 23:49:13 +0100

    added stub for detailViewController and usual table view functions

---
+++ Milestone8/Milestone8/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  Milestone8
+//
+//  Created by Alexander Tu on 02.12.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let textView = UITextView()
+        
+        
+        
+        view.addSubview(textView)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Milestone8/Milestone8/ViewController.swift
+++ Milestone8/Milestone8/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var notes = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Notes"
+        notes = ["Test", "Test2"]
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Note", for: indexPath)
+        cell.textLabel?.text = notes[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
 
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
