e8a4cf6029f7719277106faaa51c346a4f4088b1
Alexander Tu <alexander.md.tu@gmail.com>
2019-12-02 23:35:05 +0100

    started milestone8 project, a notes clone

---
+++ Milestone8/Milestone8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milestone8
+//
+//  Created by Alexander Tu on 02.12.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
