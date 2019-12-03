12bdfbc68b560ffd0ac12d35d48eccb2f5d8a6ed
Alexander Tu <alexander.md.tu@gmail.com>
2019-12-03 03:03:21 +0100

    added keyboard insetting

--- Milestone8/Milestone8/DetailViewController.swift
+++ Milestone8/Milestone8/DetailViewController.swift
@@ -43,8 +43,10 @@ class DetailViewController: UIViewController {
         // add share button
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         
-        // todo: add keyboard inset handling
-        
+        // handle keyboard change
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
     
     @objc func done() {
@@ -79,5 +81,23 @@ class DetailViewController: UIViewController {
         let vc = UIActivityViewController(activityItems: [text], applicationActivities: [])
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
+    }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            textView.contentInset = .zero
+        } else {
+            textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        textView.scrollIndicatorInsets = textView.contentInset
+
+        let selectedRange = textView.selectedRange
+        textView.scrollRangeToVisible(selectedRange)
     }
 }
