1a4b4486406793abfd3b3d1f5060bdd31a216046
Alexander Tu <alexander.md.tu@gmail.com>
2019-12-03 22:20:16 +0100

    started project23

---
+++ Project23/Project23/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Project23
+//
+//  Created by Alexander Tu on 03.12.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
