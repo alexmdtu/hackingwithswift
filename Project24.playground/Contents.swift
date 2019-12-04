6ad3cd40792c7a0de996757effb5f8e4ab94da77
Alexander Tu <alexander.md.tu@gmail.com>
2019-12-04 16:57:39 +0100

    finished challenges for project24

--- Project24.playground/Contents.swift
+++ Project24.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var str = "Hello, playground"
+extension String {
+    func withPrefix(_ prefix: String) -> String {
+        if self.hasPrefix(prefix) {
+            return self
+        } else {
+            return prefix + self
+        }
+    }
+    
+    func isNumeric() -> Bool {
+        return Double(self) != nil
+    }
+    
+    var lines: [Substring] {
+        let array = self.split(separator: "\n")
+        
+        return array
+    }
+}
+
+let test = "abcTest"
+let firstTest = test.withPrefix("abc")
+let test2 = "Test"
+let secondTest = test2.withPrefix("abc")
+
+let noNumerics = "abc"
+let hasNumerics = "22.53"
+
+let isNumericFalse = noNumerics.isNumeric()
+let isNumericTrue = hasNumerics.isNumeric()
+
+let fourLines = "this\nis\na\ntest"
+let separated = fourLines.lines
