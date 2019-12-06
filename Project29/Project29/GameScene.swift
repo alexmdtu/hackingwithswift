b6ab33b340f8d1c7057f647abc03d486ec6985b0
Alexander Tu <alexander.md.tu@gmail.com>
2019-12-06 13:46:07 +0100

    started project29

---
+++ Project29/Project29/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by Alexander Tu on 06.12.19.
+//  Copyright © 2019 Alexander Tu. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
