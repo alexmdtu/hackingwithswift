53f8f698584e6b9a0fd1bbb375bef767e4db63c3
Alexander Tu <alexander.md.tu@gmail.com>
2020-01-08 12:28:18 +0100

    initial collection view setup in storyboard

---
+++ Milestone11/Milestone11/CardCell.swift
@@ -0,0 +1,14 @@
+//
+//  CardCell.swift
+//  Milestone11
+//
+//  Created by Alexander Tu on 08.01.20.
+//  Copyright © 2020 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class CardCell: UICollectionViewCell {
+    @IBOutlet var cardText: UILabel!
+    
+}
---
+++ Milestone11/Milestone11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Milestone11
+//
+//  Created by Alexander Tu on 08.01.20.
+//  Copyright © 2020 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
