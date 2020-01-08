880f9beec3ea45d16b4fa23e4a30d60d85c5f068
Alexander Tu <alexander.md.tu@gmail.com>
2020-01-08 12:53:45 +0100

    added loading of .txt and separation into countries and capitals

--- Milestone11/Milestone11/ViewController.swift
+++ Milestone11/Milestone11/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UICollectionViewController {
 
+    var countries = [String]()
+    var capitals = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let cardContentsURL = Bundle.main.url(forResource: "capitalsCountries", withExtension: "txt") {
+            if let cardContents = try? String(contentsOf: cardContentsURL) {
+                var allPairs = cardContents.components(separatedBy: "\n")
+                allPairs.removeLast() //remove blank string array element due to last line break
+                print(allPairs)
+                countries = allPairs.map { $0.components(separatedBy: ";")[0] }
+                capitals = allPairs.map { $0.components(separatedBy: ";")[1] }
+                print(countries)
+                print(capitals)
+            }
+        }
     }
     
 
