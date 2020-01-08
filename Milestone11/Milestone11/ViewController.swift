e6b8e3d87c2a614b130c66d7f3fab76f442cbcca
Alexander Tu <alexander.md.tu@gmail.com>
2020-01-08 13:11:16 +0100

    added showing of card

--- Milestone11/Milestone11/ViewController.swift
+++ Milestone11/Milestone11/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UICollectionViewController {
 
+    var allPairs = [String]()
     var countries = [String]()
     var capitals = [String]()
     
@@ -17,9 +18,10 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // load card text
         if let cardContentsURL = Bundle.main.url(forResource: "capitalsCountries", withExtension: "txt") {
             if let cardContents = try? String(contentsOf: cardContentsURL) {
-                var allPairs = cardContents.components(separatedBy: "\n")
+                allPairs = cardContents.components(separatedBy: "\n")
                 allPairs.removeLast() //remove blank string array element due to last line break
                 print(allPairs)
                 countries = allPairs.map { $0.components(separatedBy: ";")[0] }
@@ -30,6 +32,21 @@ class ViewController: UICollectionViewController {
         }
     }
     
-
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return allPairs.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Card", for: indexPath) as? CardCell else {
+            fatalError()
+        }
+        
+        cell.layer.borderColor = UIColor.systemGray.cgColor
+        cell.layer.borderWidth = 2
+        cell.layer.cornerRadius = 7
+        cell.cardText.text = countries[indexPath.item]
+        
+        return cell
+    }
 }
 
