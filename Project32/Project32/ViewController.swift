ca32cdc62e29ef0a19b4ce156a234bd8fd41c0ea
Alexander Tu <alexander.md.tu@gmail.com>
2020-01-21 22:18:29 +0100

    started project32, initial setup done

---
+++ Project32/Project32/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project32
+//
+//  Created by Alexander Tu on 21.01.20.
+//  Copyright © 2020 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
