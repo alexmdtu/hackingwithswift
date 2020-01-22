97648830863e4c7437008a78a2f045bac34104e7
Alexander Tu <alexander.md.tu@gmail.com>
2020-01-22 23:31:29 +0100

    initial favoriting function added

--- Project32/Project32/ViewController.swift
+++ Project32/Project32/ViewController.swift
@@ -7,14 +7,114 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UITableViewController {
 
+    var projects = [[String]]()
+    var favorites = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        projects.append(["Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, FileManager, storyboards"])
+        projects.append(["Project 2: Guess the Flag", "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"])
+        projects.append(["Project 3: Social Media", "UIBarButtonItem, UIActivityViewController, the Social framework, URL"])
+        projects.append(["Project 4: Easy Browser", "loadView(), WKWebView, delegation, classes and structs, URLRequest, UIToolbar, UIProgressView., key-value observing"])
+        projects.append(["Project 5: Word Scramble", "Closures, method return values, booleans, NSRange"])
+        projects.append(["Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"])
+        projects.append(["Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"])
+        projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerated(), count, index(of:), property observers, range operators."])
+        
+        let defaults = UserDefaults.standard
+        if let savedFavorites = defaults.object(forKey: "favorites") as? [Int] {
+            favorites = savedFavorites
+        }
+        
+        tableView.isEditing = true
+        tableView.allowsSelectionDuringEditing = true
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return projects.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let project = projects[indexPath.row]
+        cell.textLabel?.attributedText = makeAttributedString(title: project[0], subtitle: project[1])
+        
+        if favorites.contains(indexPath.row) {
+            cell.editingAccessoryType = .checkmark
+        } else {
+            cell.editingAccessoryType = .none
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showTutorial(indexPath.row)
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if favorites.contains(indexPath.row) {
+            return .delete
+        } else {
+            return .insert
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .insert {
+            favorites.append(indexPath.row)
+            index(item: indexPath.row)
+        } else {
+            if let index = favorites.firstIndex(of: indexPath.row) {
+                favorites.remove(at: index)
+                deindex(item: indexPath.row)
+            }
+        }
+        
+        let defaults = UserDefaults.standard
+        defaults.set(favorites, forKey: "favorites")
+        tableView.reloadRows(at: [indexPath], with: .none)
+    }
+    
+    func makeAttributedString(title: String, subtitle: String) -> NSAttributedString {
+        let titleAttributes = [
+            NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .headline),
+            NSAttributedString.Key.foregroundColor: UIColor.purple
+        ]
+        
+        let subtitleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .subheadline)]
+        
+        let titleString = NSMutableAttributedString(string: "\(title)\n", attributes: titleAttributes)
+        let subtitleString = NSMutableAttributedString(string: subtitle, attributes: subtitleAttributes)
+        
+        titleString.append(subtitleString)
+        
+        return titleString
+    }
 
+    func showTutorial(_ which: Int) {
+        if let url = URL(string: "https://www.hackingwithswift.com/read/\(which + 1)") {
+            let config = SFSafariViewController.Configuration()
+            config.entersReaderIfAvailable = true
+            
+            let vc = SFSafariViewController(url: url, configuration: config)
+            present(vc, animated: true)
+        }
+    }
+    
+    func index(item: Int) {
+        
+    }
+    
+    func deindex(item: Int) {
+        
+    }
 }
 
