d54a04d413bf3ca2a444ed8ad3367481854c2139
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-21 16:52:42 +0100

    added player with animation

--- Project36-CrashyPlane/Project36-CrashyPlane/GameScene.swift
+++ Project36-CrashyPlane/Project36-CrashyPlane/GameScene.swift
@@ -10,7 +10,29 @@ import GameplayKit
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMove(to view: SKView) {}
+    var player: SKSpriteNode!
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {}
+    override func didMove(to view: SKView) {
+        createPlayer()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    func createPlayer() {
+        let playerTexture = SKTexture(imageNamed: "player-1")
+        player = SKSpriteNode(texture: playerTexture)
+        player.zPosition = 10
+        player.position = CGPoint(x: frame.width / 6, y: frame.height * 0.75)
+        
+        addChild(player)
+        
+        let frame2 = SKTexture(imageNamed: "player-2")
+        let frame3 = SKTexture(imageNamed: "player-3")
+        let animation = SKAction.animate(with: [playerTexture, frame2, frame3, frame3], timePerFrame: 0.01)
+        let runForever = SKAction.repeatForever(animation)
+        
+        player.run(runForever)
+    }
 }
