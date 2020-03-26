92aeab1814ec41e75fad47ef9722513375467b45
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-26 16:39:58 +0100

    started project and added assets

---
+++ Project37-PsychicTester/Project37-PsychicTester WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Project37-PsychicTester WatchKit Extension
+//
+//  Created by Alexander Tu on 26.03.20.
+//  Copyright © 2020 Alexander Tu. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
