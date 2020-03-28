173ce8acc41c7190a53ce9fe666775fe106cd4bf
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-28 23:14:29 +0100

    initial setup

--- Project37-PsychicTester/Project37-PsychicTester/ViewController.swift
+++ Project37-PsychicTester/Project37-PsychicTester/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, WCSessionDelegate {
     var allCards = [CardViewController]()
     var music: AVAudioPlayer!
     var lastMessage: CFAbsoluteTime = 0
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -37,6 +37,15 @@ class ViewController: UIViewController, WCSessionDelegate {
             session.delegate = self
             session.activate()
         }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let instructions = "Please ensure your Apple Watch is configured correctly. On your iPhone, launch Apple's 'Watch' configuration app then choose General > Wake Screen. On that screen, please disable Wake Screen On Wrist Raise, then select Wake For 70 Seconds. On your Apple Watch, please swipe up on your watch face and enable Silent Mode. You're done!"
+        let ac = UIAlertController(title: "Adjust your settings", message: instructions, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "I'm Ready", style: .default))
+        present(ac, animated: true)
     }
 
     @objc func loadCards() {
@@ -175,7 +184,7 @@ class ViewController: UIViewController, WCSessionDelegate {
     func sessionDidBecomeInactive(_ session: WCSession) {}
 
     func sessionDidDeactivate(_ session: WCSession) {}
-    
+
     func sendWatchMessage() {
         let currentTime = CFAbsoluteTimeGetCurrent()
 
@@ -185,7 +194,7 @@ class ViewController: UIViewController, WCSessionDelegate {
         }
 
         // send a message to the watch if it's reachable
-        if (WCSession.default.isReachable) {
+        if WCSession.default.isReachable {
             // this is a meaningless message, but it's enough for our purposes
             let message = ["Message": "Hello"]
             WCSession.default.sendMessage(message, replyHandler: nil)
---
+++ Project38-GitHubCommits/Project38-GitHubCommits/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Project38-GitHubCommits
+//
+//  Created by Alexander Tu on 28.03.20.
+//  Copyright © 2020 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
