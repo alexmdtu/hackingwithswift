c972cd8f39245dc8d8142b6ab106644123e83497
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-31 21:20:17 +0200

    added a few filters for the commits

--- Project38-GitHubCommits/Project38-GitHubCommits/ViewController.swift
+++ Project38-GitHubCommits/Project38-GitHubCommits/ViewController.swift
@@ -12,21 +12,24 @@ import UIKit
 class ViewController: UITableViewController {
     var container: NSPersistentContainer!
     var commits = [Commit]()
+    var commitPredicate: NSPredicate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(changeFilter))
+        
         container = NSPersistentContainer(name: "Project38")
         container.loadPersistentStores { _, error in
             self.container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
-            
+
             if let error = error {
                 print("Unresolved eror \(error)")
             }
         }
 
         performSelector(inBackground: #selector(fetchCommits), with: nil)
-        
+
         loadSavedData()
     }
 
@@ -70,11 +73,12 @@ class ViewController: UITableViewController {
         let formatter = ISO8601DateFormatter()
         commit.date = formatter.date(from: json["commit"]["committer"]["date"].stringValue) ?? Date()
     }
-    
+
     func loadSavedData() {
         let request = Commit.createFetchRequest()
         let sort = NSSortDescriptor(key: "date", ascending: false)
         request.sortDescriptors = [sort]
+        request.predicate = commitPredicate
 
         do {
             commits = try container.viewContext.fetch(request)
@@ -85,22 +89,48 @@ class ViewController: UITableViewController {
         }
     }
     
+    @objc func changeFilter() {
+        let ac = UIAlertController(title: "Filter commits…", message: nil, preferredStyle: .actionSheet)
+
+        ac.addAction(UIAlertAction(title: "Show only fixes", style: .default) { [unowned self] _ in
+            self.commitPredicate = NSPredicate(format: "message CONTAINS[c] 'fix'")
+            self.loadSavedData()
+        })
+        ac.addAction(UIAlertAction(title: "Ignore Pull Requests", style: .default) { [unowned self] _ in
+            self.commitPredicate = NSPredicate(format: "NOT message BEGINSWITH 'Merge pull request'")
+            self.loadSavedData()
+        })
+        ac.addAction(UIAlertAction(title: "Show only recent", style: .default) { [unowned self] _ in
+            let twelveHoursAgo = Date().addingTimeInterval(-43200)
+            self.commitPredicate = NSPredicate(format: "date > %@", twelveHoursAgo as NSDate)
+            self.loadSavedData()
+        })
+        ac.addAction(UIAlertAction(title: "Show all commits", style: .default) { [unowned self] _ in
+            self.commitPredicate = nil
+            self.loadSavedData()
+        })
+
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+
     // MARK: - Table View Functions
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return commits.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Commit", for: indexPath)
-        
+
         let commit = commits[indexPath.row]
         cell.textLabel?.text = commit.message
         cell.detailTextLabel?.text = commit.date.description
-        
+
         return cell
     }
 }
