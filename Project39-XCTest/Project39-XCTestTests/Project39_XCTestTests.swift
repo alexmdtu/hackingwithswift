154973ce195c25869ae7c20177789b5abdb4cb03
Alexander Tu <alexander.md.tu@gmail.com>
2020-04-01 23:03:35 +0200

    added counting frequency of words and displaying it as detail text

--- Project39-XCTest/Project39-XCTestTests/Project39_XCTestTests.swift
+++ Project39-XCTest/Project39-XCTestTests/Project39_XCTestTests.swift
@@ -20,6 +20,13 @@ class Project39_XCTestTests: XCTestCase {
     
     func testAllWordsLoaded() {
         let playData = PlayData()
-        XCTAssertEqual(playData.allWords.count, 384001, "allWords must be 384001")
+        XCTAssertEqual(playData.allWords.count, 18440, "allWords must be 18440")
+    }
+    
+    func testWordCountsAreCorrect() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.wordCounts["home"], 174, "Home does not appear 174 times")
+        XCTAssertEqual(playData.wordCounts["fun"], 4, "Fun does not appear 4 times")
+        XCTAssertEqual(playData.wordCounts["mortal"], 41, "Mortal does not appear 41 times")
     }
 }
