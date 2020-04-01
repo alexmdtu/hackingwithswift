d3d0ffe969e15e39279984a6426bd7ec2cf9dbe3
Alexander Tu <alexander.md.tu@gmail.com>
2020-04-01 22:05:24 +0200

    initial setup

---
+++ Project39-XCTest/Project39-XCTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project39-XCTest
+//
+//  Created by Alexander Tu on 01.04.20.
+//  Copyright © 2020 Alexander Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
