91602658bac99a4ae03bc2302bb1381515972850
Alexander Tu <alexander.md.tu@gmail.com>
2020-04-02 15:16:16 +0200

    added UI tests

--- Project39-XCTest/Project39-XCTestUITests/Project39_XCTestUITests.swift
+++ Project39-XCTest/Project39-XCTestUITests/Project39_XCTestUITests.swift
@@ -9,10 +9,9 @@
 import XCTest
 
 class Project39_XCTestUITests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        XCUIApplication().launch()
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
@@ -23,21 +22,21 @@ class Project39_XCTestUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInitialStateIsCorrect() {
+        let table = XCUIApplication().tables
+        XCTAssertEqual(table.cells.count, 7, "There should be 7 rows initially")
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testUserFilteringByString() {
+        let app = XCUIApplication()
+        app.navigationBars["Project39_XCTest.View"].buttons["Search"].tap()
+        let filterAlert = app.alerts
+        let textField = filterAlert.textFields.element
+        textField.typeText("test")
+
+        filterAlert.buttons["Filter"].tap()
+
+        XCTAssertEqual(app.tables.cells.count, 56, "There should be 56 words matching 'test'")
+
     }
 }
